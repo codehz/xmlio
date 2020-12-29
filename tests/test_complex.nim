52a46c5d7693e78536776db4971b26d945b2eeeb
CodeHz <codehz@outlook.com>
2020-12-29 15:55:29 +0800

    update tests

--- tests/test_complex.nim
+++ tests/test_complex.nim
@@ -11,48 +11,39 @@ trait Repository:
 
 registerTypeId(ref Repository, "e71dc461-8af1-4206-a3a4-d5fe55985fd7")
 
-type RootDocument = object of RootObj
-  version: int
-  children: seq[ref Repository]
+declareXmlElement:
+  type RootDocument {.id: "9afd3387-f6c9-4361-a279-e87b26862140".} = object of RootObj
+    version {.check(value != 1, r"version mismatch").}: int
+    children: seq[ref Repository]
 
-generateXmlElementHandler RootDocument, "9afd3387-f6c9-4361-a279-e87b26862140":
-  if self.version != 1: raise newException(ValueError, "version mismatch")
-
-type LocalRepo = object of RootObj
-  name: string
-  path: string
+declareXmlElement:
+  type LocalRepo {.id: "f11bdf51-eb0b-4d2d-9d2c-e0e0394cb64c".} = object of RootObj
+    name {.check(value == "", r"name is empty").}: string
+    path {.check(value == "", r"path is empty").}: string
 
 impl LocalRepo, Repository:
   method getName(self: ref LocalRepo): string = self.name
   method fetchPackageLists(self: ref LocalRepo): seq[string] = @[]
 
-generateXmlElementHandler LocalRepo, "f11bdf51-eb0b-4d2d-9d2c-e0e0394cb64c":
-  if self.name == "": raise newException(ValueError, "name is empty")
-  if self.path == "": raise newException(ValueError, "path is empty")
-
-type RemoteRepo = object of RootObj
-  name: string
-  url: string
+declareXmlElement:
+  type RemoteRepo {.id: "22187be0-4aea-4b78-9a73-fc03c7b948c5".} = object of RootObj
+    name {.check(value == "", r"name is empty").}: string
+    url {.check(value == "", r"url is empty").}: string
 
 impl RemoteRepo, Repository:
   method getName(self: ref RemoteRepo): string = self.name
   method fetchPackageLists(self: ref RemoteRepo): seq[string] = @[]
 
-generateXmlElementHandler RemoteRepo, "22187be0-4aea-4b78-9a73-fc03c7b948c5":
-  if self.name == "": raise newException(ValueError, "name is empty")
-  if self.url == "": raise newException(ValueError, "url is empty")
-
-type PrefixRepo = object of RootObj
-  prefix: string
-  children: ref Repository
+declareXmlElement:
+  type PrefixRepo {.
+      id: "a701c97a-45f2-4326-96af-58808fdc49fe",
+      children: child.} = object of RootObj
+    prefix {.check(value == "", r"prefix is empty").}: string
+    child {.check(value == nil, r"no children").}: ref Repository
 
 impl PrefixRepo, Repository:
-  method getName(self: ref PrefixRepo): string = self.prefix & self.children.getName()
-  method fetchPackageLists(self: ref PrefixRepo): seq[string] = self.children.fetchPackageLists()
-
-generateXmlElementHandler PrefixRepo, "a701c97a-45f2-4326-96af-58808fdc49fe":
-  if self.prefix == "": raise newException(ValueError, "prefix is empty")
-  if self.children == nil: raise newException(ValueError, "no children")
+  method getName(self: ref PrefixRepo): string = self.prefix & self.child.getName()
+  method fetchPackageLists(self: ref PrefixRepo): seq[string] = self.child.fetchPackageLists()
 
 var registry = newSimpleRegistry()
 var rootns = newSimpleXmlnsHandler()
